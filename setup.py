20fc156b920833debbff5c6aaf75cdd2097b1722
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-11-18 17:32:01 +0100

    project base

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of the emotional valence of political adds from ProPublica facebook political ads data.',
+    author='Tuomo Kareoja',
+    license='MIT',
+)
