b99db2c01d62d5e3aad258585e1143b5e77a5484
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-11-21 15:52:23 +0100

    sentiment with normalization

--- notebooks/sentiment_analysis.py
+++ notebooks/sentiment_analysis.py
@@ -20,31 +20,47 @@ df = pd.read_csv(
 
 # %%
 
-df.sentiment_afinn.describe()
+df.sentiment.describe()
 
-sns.distplot(df.sentiment_afinn)
+sns.distplot(df.sentiment)
 plt.title("Afinn sentiment distribution")
 plt.show()
 
 # %%
 
-df.sentiment_vader.describe()
+df.sentiment_norm.describe()
 
-sns.distplot(df.sentiment_vader)
-plt.title("Vader sentiment distribution")
+sns.distplot(df.sentiment_norm)
+plt.title("Afinn normalized sentiment distribution")
 plt.show()
 
 # %%
 
 
-ax = sns.scatterplot(df.sentiment_afinn, df.sentiment_vader, alpha=0.005)
+ax = sns.scatterplot(df.sentiment, df.sentiment_norm, alpha=0.005)
+plt.title("Relationship between Afinn and Afinn normalized")
 plt.show()
 
+# %% [markdown]
+
+# # What do the most positive and negative messages look like
+#
+# * For pure afinn sentiment values it is clear that really long and messages get the
+# top spots
+# * With normalized (divided by the number of words) sentiment values short and clear
+# messages get the top spots. This seems much better
+# * The normalized sentiment values have no outliers and the values seems more
+# realistic and intuitive. We only used normalized values for the rest of the
+# analysis
+
 # %%
+
+print("AFINN")
+print("")
 
 print("POSITIVE:")
 for i, text in enumerate(
-    df.sort_values(by="sentiment_afinn", ascending=False)["message"][:5]
+    df.sort_values(by="sentiment", ascending=False)["message"][:5]
 ):
     print(i + 1, "most positive")
     print(text[:200])
@@ -52,18 +68,18 @@ for i, text in enumerate(
 
 print("")
 print("NEGATIVE:")
-for i, text in enumerate(
-    df.sort_values(by="sentiment_afinn", ascending=True)["message"][:5]
-):
+for i, text in enumerate(df.sort_values(by="sentiment", ascending=True)["message"][:5]):
     print(i + 1, "most negative")
     print(text[:200])
     print("")
 
-# %%
+print("AFINN normalized")
+print("")
+
 
 print("POSITIVE:")
 for i, text in enumerate(
-    df.sort_values(by="sentiment_vader", ascending=False)["message"][:5]
+    df.sort_values(by="sentiment_norm", ascending=False)["message"][:5]
 ):
     print(i + 1, "most positive")
     print(text[:200])
@@ -72,7 +88,7 @@ for i, text in enumerate(
 print("")
 print("NEGATIVE:")
 for i, text in enumerate(
-    df.sort_values(by="sentiment_vader", ascending=True)["message"][:5]
+    df.sort_values(by="sentiment_norm", ascending=True)["message"][:5]
 ):
     print(i + 1, "most negative")
     print(text[:200])
@@ -81,33 +97,17 @@ for i, text in enumerate(
 # %%
 
 afinn_positive_wc = WordCloud(background_color="white").generate(
-    " ".join(df.sort_values(by="sentiment_afinn", ascending=False)["message"][:500])
+    " ".join(df.sort_values(by="sentiment_norm", ascending=False)["message"][:500])
 )
 plt.imshow(afinn_positive_wc)
-plt.title("Wordcloud of 500 most positive messages by Afinn")
+plt.title("Wordcloud of 500 most positive messages")
 plt.show()
 
 afinn_negative_wc = WordCloud(background_color="white").generate(
-    " ".join(df.sort_values(by="sentiment_afinn", ascending=True)["message"][:500])
+    " ".join(df.sort_values(by="sentiment_norm", ascending=True)["message"][:500])
 )
 plt.imshow(afinn_negative_wc)
-plt.title("Wordcloud of 500 most negative messages by Afinn")
-plt.show()
-
-# %%
-
-vader_positive_wc = WordCloud(background_color="white").generate(
-    " ".join(df.sort_values(by="sentiment_vader", ascending=False)["message"][:500])
-)
-plt.imshow(vader_positive_wc)
-plt.title("Wordcloud of 500 most positive messages by Vader")
-plt.show()
-
-vader_negative_wc = WordCloud(background_color="white").generate(
-    " ".join(df.sort_values(by="sentiment_vader", ascending=True)["message"][:500])
-)
-plt.imshow(vader_negative_wc)
-plt.title("Wordcloud of 500 most negative messages by Vader")
+plt.title("Wordcloud of 500 most negative messages")
 plt.show()
 
 
@@ -126,23 +126,17 @@ df["updated_at_month"] = df["updated_at_month"].dt.date
 
 # %%
 
-df.groupby("created_at_date")["sentiment_afinn"].mean().plot()
-plt.show()
-
-df.groupby("created_at_date")["sentiment_vader"].mean().plot()
+df.groupby("created_at_date")["sentiment_norm"].mean().plot()
 plt.show()
 
 # %%
 
-sns.boxplot(x="created_at_month", y="sentiment_afinn", data=df)
+sns.boxplot(
+    x="created_at_date", y="sentiment_norm", data=df.sort_values(by="created_at_date")
+)
+plt.title("Distribution of Afinn be date")
 plt.xticks(rotation=90)
-plt.ylim([-15, 15])
-plt.show()
-
-# %%
-
-sns.boxplot(x="created_at_month", y="sentiment_vader", data=df)
-plt.xticks(rotation=90)
+plt.ylim([-0.4, 0.4])
 plt.show()
 
 # %%
@@ -150,26 +144,57 @@ plt.show()
 df_sample = df.sample(10000)
 plt.scatter(
     x=df_sample.created_at_date,
-    y=df_sample.sentiment_afinn,
-    c=df_sample.sentiment_afinn,
-    s=df_sample.impressions * 100,
+    y=df_sample.sentiment_norm,
+    c=df_sample.sentiment_norm,
+    # s=df_sample.impressions * 100,
     alpha=0.01,
 )
-plt.xticks(rotation=90)
-plt.xlim([pd.Timestamp("2018-01-01"), pd.Timestamp("2019-01-01")])
-plt.ylim([-30, 30])
+plt.xticks(rotation=90) 
+plt.ylim([-0.5, 0.5])
+plt.xlim([pd.Timestamp("2018-09-15"), pd.Timestamp("2018-11-15")])
+plt.show()
+
+# %%
+
+top_segments = df.target_segment.value_counts().index[0:5]
+df_top_segments = df[df["target_segment"].isin(top_segments)]
+
+sns.lineplot(
+    x="created_at_date", y="sentiment_norm", hue="target_segment", data=df_top_segments
+)
+plt.xlim([pd.Timestamp("2018-09-15"), pd.Timestamp("2018-11-15")])
+plt.show()
+
+# %%
+
+sns.boxplot(x="target_segment", y="sentiment_norm", data=df_top_segments)
 plt.show()
 
 # %%
 
 df_sample = df.sample(10000)
 plt.scatter(
-    x=df_sample.created_at_date,
-    y=df_sample.sentiment_vader,
-    c=df_sample.sentiment_vader,
-    s=df_sample.impressions * 100,
+    x=df_sample.message.str.len(),
+    y=df_sample.sentiment_norm,
+    c=df_sample.sentiment_norm,
+    # s=df_sample.impressions * 100,
     alpha=0.01,
-)
+) 
+plt.ylim([-0.5, 0.5])
+plt.xlim([0, 1000])
 plt.xticks(rotation=90)
-plt.xlim([pd.Timestamp("2018-01-01"), pd.Timestamp("2019-01-01")])
 plt.show()
+
+# %%
+
+sns.boxplot(
+    x=df.targetedness,
+    y=df.sentiment_norm,
+) 
+plt.ylim([-0.5, 0.5])
+# plt.xticks(rotation=90)
+plt.show()
+
+
+# %%
+
